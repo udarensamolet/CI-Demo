6ede84590cf54a844bf3cafdf17a06e8ef12e9ae
udarensamolet <udarensamolet@yahoo.com>
2024-07-26 06:34:48 +0300

    Updated test files for headless mode of Chrome

--- HTML_Elements_01/FillForm.cs
+++ HTML_Elements_01/FillForm.cs
@@ -1,6 +1,8 @@
-using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using System;
 
 namespace HTMLElements01
 {
@@ -11,7 +13,7 @@ namespace HTMLElements01
         [SetUp]
         public void Setup()
         {
-             ChromeOptions options = new ChromeOptions();
+            ChromeOptions options = new ChromeOptions();
             // Ensure Chrome runs in headless mode
             options.AddArguments("headless");
             // Bypass OS security model, required for running in Docker
@@ -36,56 +38,54 @@ namespace HTMLElements01
         [Test]
         public void Test_RegisterUser()
         {
-            // Click on My Account link
-            driver.FindElement(By.LinkText("My Account")).Click();
+            try
+            {
+                Console.WriteLine("Navigating to My Account");
+                driver.FindElement(By.LinkText("My Account")).Click();
 
-            // Click on Continue button
-            driver.FindElement(By.LinkText("Continue")).Click();
+                Console.WriteLine("Clicking Continue button");
+                driver.FindElement(By.LinkText("Continue")).Click();
 
-            // Fill in the form
-            driver.FindElement(By.CssSelector("input[type='radio'][value='f']")).Click();
-            driver.FindElement(By.Name("firstname")).SendKeys("Fiona");
-            driver.FindElement(By.Name("lastname")).SendKeys("Apple");
-            driver.FindElement(By.Id("dob")).SendKeys("09/13/1977");
+                Console.WriteLine("Filling in the form");
+                driver.FindElement(By.CssSelector("input[type='radio'][value='f']")).Click();
+                driver.FindElement(By.Name("firstname")).SendKeys("Fiona");
+                driver.FindElement(By.Name("lastname")).SendKeys("Apple");
+                driver.FindElement(By.Id("dob")).SendKeys("09/13/1977");
 
-            // Generate a unique email address
-            Random rnd = new Random();
-            // Generate a random number between 1000 and 9999
-            int num = rnd.Next(1000, 9999);  
-            String email = "fiona.apple" + num.ToString() + "@example.com";
+                Random rnd = new Random();
+                int num = rnd.Next(1000, 9999);
+                String email = "fiona.apple" + num.ToString() + "@example.com";
+                driver.FindElement(By.Name("email_address")).SendKeys(email);
+                driver.FindElement(By.Name("company")).SendKeys("Example Inc.");
+                driver.FindElement(By.Name("street_address")).SendKeys("123 Main Str.");
+                driver.FindElement(By.Name("suburb")).SendKeys("London");
+                driver.FindElement(By.Name("postcode")).SendKeys("223242");
+                driver.FindElement(By.Name("city")).SendKeys("London");
+                driver.FindElement(By.Name("state")).SendKeys("London");
 
-            driver.FindElement(By.Name("email_address")).SendKeys(email);
-            driver.FindElement(By.Name("company")).SendKeys("Example Inc.");
-            driver.FindElement(By.Name("street_address")).SendKeys("123 Main Str.");
-            driver.FindElement(By.Name("suburb")).SendKeys("London");
-            driver.FindElement(By.Name("postcode")).SendKeys("223242");
-            driver.FindElement(By.Name("city")).SendKeys("London");
-            driver.FindElement(By.Name("state")).SendKeys("London");
+                new SelectElement(driver.FindElement(By.Name("country"))).SelectByText("United Kingdom");
+                driver.FindElement(By.Name("telephone")).SendKeys("2432424112");
+                driver.FindElement(By.Name("newsletter")).Click();
+                driver.FindElement(By.Name("password")).SendKeys("fiona_123456");
+                driver.FindElement(By.Name("confirmation")).SendKeys("fiona_123456");
 
-            // Select country from dropdown
-            new SelectElement(driver.FindElement(By.Name("country"))).SelectByText("United Kingdom");
+                Console.WriteLine("Submitting the form");
+                driver.FindElement(By.Id("tdb4")).Submit();
 
-            driver.FindElement(By.Name("telephone")).SendKeys("2432424112");
-            driver.FindElement(By.Name("newsletter")).Click();
+                Console.WriteLine("Checking for account creation success message");
+                bool isAccountCreated = driver.PageSource.Contains("Your Account Has Been Created!");
+                Assert.IsTrue(isAccountCreated, "Account creation failed.");
+                Console.WriteLine("User Account Created with email: " + email);
 
-            driver.FindElement(By.Name("password")).SendKeys("fiona_123456");
-            driver.FindElement(By.Name("confirmation")).SendKeys("fiona_123456");
-
-
-            // Submit the form
-            driver.FindElement(By.Id("tdb4")).Submit();
-
-            // Assert account creation success
-            Assert.IsTrue(driver.PageSource.Contains("Your Account Has Been Created!"), "Account creation failed.");
-            
-            // Click on Log Off link
-            driver.FindElement(By.LinkText("Log Off")).Click();
-
-            // Click on Continue button
-            driver.FindElement(By.LinkText("Continue")).Click();
-
-            Console.WriteLine("User Account Created with email: " + email);
-
+                Console.WriteLine("Logging off");
+                driver.FindElement(By.LinkText("Log Off")).Click();
+                driver.FindElement(By.LinkText("Continue")).Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred: " + ex.Message);
+                throw;
+            }
         }
 
         [TearDown]
@@ -94,5 +94,4 @@ namespace HTMLElements01
             driver.Quit();
         }
     }
-    
 }
